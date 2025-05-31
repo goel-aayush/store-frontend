22a49909b3e1b5843da1bc2042243bc6df2a76a1
aayush <mgoel1296@gmail.com>
2025-05-31 17:47:08 +0530

    update

--- src/modules/core/axiosauth/axiosConfig.js
+++ src/modules/core/axiosauth/axiosConfig.js
@@ -1,21 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://store-management-nyeh.onrender.com', 
+  baseURL: "http://localhost:5001",
 });
-//http://localhost:5001
+//
+//https://store-management-nyeh.onrender.com
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`; 
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default axiosInstance;
